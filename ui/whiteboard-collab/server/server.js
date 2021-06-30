91c3d07ab96d092fb7963a55f01d1893f5a6ac12
Pourush1999 <pourushkhanna@gmail.com>
2021-06-30 20:55:34 +0530

    commit

--- ui/whiteboard-collab/server/server.js
+++ ui/whiteboard-collab/server/server.js
@@ -1,6 +1,9 @@
 var app = require('express')();
+var express= require('express');
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var cors = require("cors");
+app.use(cors());
 
 io.on('connection', (socket)=> {
       console.log('User Online');
@@ -13,13 +16,12 @@ io.on('connection', (socket)=> {
 const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'collaborative-whiteboard-master/ui/whiteboard-collab/build')));
+  app.use(express.static(path.join(__dirname, 'whiteboard-collab/build')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'collaborative-whiteboard-master/ui/whiteboard-collab/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'whiteboard-collab/build', 'index.html'));
   });
 }
-
 var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
 http.listen(server_port, () => {
     console.log("Started on : "+ server_port);
