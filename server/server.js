cfd80e95c26599541d12f11fb9d81ea5f64d27df
Pourush1999 <pourushkhanna@gmail.com>
2021-06-30 14:14:40 +0530

    commit

---
+++ server/server.js
@@ -0,0 +1,26 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+io.on('connection', (socket)=> {
+      console.log('User Online');
+
+      socket.on('canvas-data', (data)=> {
+            socket.broadcast.emit('canvas-data', data);
+            
+      })
+})
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'collaborative-whiteboard-master/ui/whiteboard-collab/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'collaborative-whiteboard-master/ui/whiteboard-collab/build', 'index.html'));
+  });
+}
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
+http.listen(server_port, () => {
+    console.log("Started on : "+ server_port);
+})
